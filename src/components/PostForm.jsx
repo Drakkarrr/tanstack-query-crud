740fe8335d80541b068f54147b475e725c0476b7
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-25 03:00:24 +0800

    ui: basic edit form component

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,5 +1,11 @@
 const PostForm = () => {
-  return <div>PostForm</div>;
+  const renderField = (label) => (
+    <div>
+      <label>{label}</label> <input type='text' name={label.toLowerCase()} />
+    </div>
+  );
+
+  return <form>{renderField('Title')}</form>;
 };
 
 export default PostForm;
--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,9 +1,11 @@
+import { useParams } from 'react-router-dom';
 import PostForm from '../components/PostForm';
 
 const EditPost = () => {
+  const { id } = useParams();
   return (
     <div>
-      EditPost
+      EditPost {id}
       <PostForm />
     </div>
   );
