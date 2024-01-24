3d53b297e1c9f13c0e00e0edf1e4480ea70f845e
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-25 03:05:17 +0800

    post state added

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
 const PostForm = () => {
+  const [post, setPost] = useState({ title: '', body: '' });
+
   const renderField = (label) => (
     <div>
-      <label>{label}</label> <input type='text' name={label.toLowerCase()} />
+      <label>{label}</label>{' '}
+      <input
+        type='text'
+        name={label.toLowerCase()}
+        value={post[label.toLowerCase()]}
+      />
     </div>
   );
+  console.log(post);
 
-  return <form>{renderField('Title')}</form>;
+  return (
+    <form>
+      {renderField('Title')}
+      {renderField('Body')}
+    </form>
+  );
 };
 
 export default PostForm;
