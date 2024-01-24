2a88c48e2d872e2775f8e78187c50aec92a85871
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-25 02:47:25 +0800

    initial ui for post list page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import PostListPage from './pages/PostListPage';
 
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
+
+      <Routes>
+        <Route path='/' element={<PostListPage />} />
+      </Routes>
     </>
   );
 }
