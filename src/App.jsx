254701dd3e74fdeea4bc7a73c90486f2f0a40a8c
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-25 02:50:07 +0800

    individual post with params

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import PostListPage from './pages/PostListPage';
+import Post from './pages/Post';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <h1>Hello World!</h1>
 
       <Routes>
+        <Route path='/' element={<PostListPage />} />
+        <Route path='/post/:id' element={<Post />} />
         <Route path='/' element={<PostListPage />} />
       </Routes>
     </>
