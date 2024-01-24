f4269191b27e208e19916bf0bce0578b7d31ad04
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-25 04:06:46 +0800

    successfully setup mock backend and fetch data

--- src/pages/PostListPage.jsx
+++ src/pages/PostListPage.jsx
@@ -3,19 +3,30 @@ import AddPost from '../components/AddPost';
 import { useQuery } from '@tanstack/react-query';
 
 const PostListPage = () => {
-  const { data, isLoading, isError, error } = useQuery({
+  const {
+    data: posts,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ['posts'],
     queryFn: fetchPosts,
   });
 
-  console.log(data);
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>{error.message}</div>;
 
   return (
-    <div>
+    <>
       <AddPost />
       <br />
-      List Here
-    </div>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </>
   );
 };
 
