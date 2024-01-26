f93586a7af779cf8d60db5abcac4e1fd5b7601ad
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-26 21:01:24 +0800

    api: fetch individual post request handler

--- src/api/posts.js
+++ src/api/posts.js
@@ -2,3 +2,8 @@ export const fetchPosts = async () => {
   const response = await fetch('http://localhost:3000/posts');
   return response.json();
 };
+
+export const fetchPost = async (id) => {
+  const response = await fetch(`http://localhost:3000/posts/${id}`);
+  return response.json();
+};
