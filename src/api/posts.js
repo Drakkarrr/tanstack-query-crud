f136be6bb2a67de59852c86c0fcdf19ca3a9b4aa
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-28 08:44:32 +0800

    added create post handler for api

--- src/api/posts.js
+++ src/api/posts.js
@@ -7,3 +7,8 @@ export const fetchPost = async (id) => {
   const response = await fetch(`http://localhost:3000/posts/${id}`);
   return response.json();
 };
+
+export const createhPost = async (id) => {
+  const response = await fetch(`http://localhost:3000/posts/${id}`);
+  return response.json();
+};
