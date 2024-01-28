46c2cc7cf20bc143626c8d64c57ea07d15832919
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-28 08:47:09 +0800

    fet: constructed fcreate post api handler

--- src/api/posts.js
+++ src/api/posts.js
@@ -8,7 +8,13 @@ export const fetchPost = async (id) => {
   return response.json();
 };
 
-export const createhPost = async (id) => {
-  const response = await fetch(`http://localhost:3000/posts/${id}`);
+export const createhPost = async (newPost) => {
+  const response = await fetch(`http://localhost:3000/posts/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newPost),
+  });
   return response.json();
 };
