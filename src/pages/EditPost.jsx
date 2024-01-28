21cd7bc37a05eac23315ebbd639ce8871f756155
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-29 06:11:00 +0800

    setup query navigate and func fetchpost tanstack

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,8 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import PostForm from '../components/PostForm';
 
 const EditPost = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
+
+  const {
+    data: post,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['posts', id],
+    queryFn: () => fetchPost(id),
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>{error.message}</div>;
+
   return (
     <div>
       EditPost {id}
