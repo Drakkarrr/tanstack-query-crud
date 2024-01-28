d94fc01cba417307a39fc7026e957511c1bef410
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-29 06:13:04 +0800

    check response if exist and added importtants imports

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,5 +1,7 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import PostForm from '../components/PostForm';
+import { fetchPost } from '../api/posts';
+import { useQuery } from '@tanstack/react-query';
 
 const EditPost = () => {
   const navigate = useNavigate();
@@ -17,6 +19,8 @@ const EditPost = () => {
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>{error.message}</div>;
+
+  console.log(post);
 
   return (
     <div>
