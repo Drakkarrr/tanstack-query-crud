4caaaa9d66a7edf7818d0856379f8cab3b11e05e
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-28 08:43:48 +0800

    todo: to be added mutation

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,6 +1,11 @@
+import { useMutation } from '@tanstack/react-query';
 import PostForm from './PostForm';
 
 const AddPost = () => {
+  const createPostMutation = useMutation({
+    mutationFn: 
+  });
+
   return (
     <>
       <h2>Add New Post</h2>
