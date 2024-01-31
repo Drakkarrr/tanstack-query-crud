266f8874a5776a1b2452c1fa7e3f07694568746e
Drakkarrr <drakkar.alpha00@gmail.com>
2024-01-31 08:35:52 -0800

    created update post fetch api handler function

--- src/api/posts.js
+++ src/api/posts.js
@@ -18,3 +18,14 @@ export const createPost = async (newPost) => {
   });
   return response.json();
 };
+
+export const updatePost = async (updatedPost) => {
+  const response = await fetch(`http://localhost:3000/posts/${updatePost.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updatePost),
+  });
+  return response.json();
+};
